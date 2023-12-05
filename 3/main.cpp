ad7ec0d26a13a5cc2e68ee9a2b199170f0ea29ca
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-12-05 22:00:23 +0100

    feat: solve challenge 3 part 1

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,6 +2,8 @@
 #include "../inputReader.h"
 #include <vector>
 #include <list>
+#include <numeric>
+#include <algorithm>
 
 static const std::string INPUT_FILE_PATH = "input.txt";
 static const char SPACING_CHAR = '.';
@@ -32,7 +34,6 @@ int main(int argc, char *argv[])
         input.push_back(lineVector);
     }
 
-    int sum = 0;
     std::vector<std::pair<int, int>> beginningOfNumberCoords;
 
     // Rows
@@ -44,35 +45,39 @@ int main(int argc, char *argv[])
             const char c = input[i][j];
             if (std::isdigit(c) || c == SPACING_CHAR)
             {
-                std::cout << "continueBefore: " << c << std::endl;
                 continue;
             }
-            std::cout << "c: " << c << std::endl;
 
             // hier sind jetzt nur noch die Symbole
             // um das Symbol schauen, wo Zahl ist
             // m = row
-            for (int m = i - 1; m < i + 2; m++)
+            for (int m : {i - 1, i, i + 1})
             {
-                std::cout << "m: " << m << std::endl;
+                // std::cout << "m: " << m << std::endl;
                 // n = columns
-                for (int n = j - 1; n < j + 2; n++)
+                for (int n : {j - 1, j, j + 1})
                 {
-                    std::cout << "n: " << n << std::endl;
+                    // std::cout << "n: " << n << std::endl;
                     if ((m < 0 || m >= input.size()) ||
                         (n < 0 || n >= input[m].size()) ||
                         (!std::isdigit(input[m][n])))
                     {
-                        std::cout << "continue" << std::endl;
                         continue;
                     }
 
                     // warum -1????
                     // diesen Part verstehe ich generell nicht ganz
-                    // while (n > 0 && std::isdigit(input[m][n - 1]))
-                    // {
-                    //     n--;
-                    // }
+                    while (n > 0 && std::isdigit(input[m][n - 1]))
+                    {
+                        n--;
+                    }
+
+                    // no duplicate coords
+                    auto it = std::find(beginningOfNumberCoords.begin(), beginningOfNumberCoords.end(), std::pair<int, int>(m, n));
+                    if (it != beginningOfNumberCoords.end())
+                    {
+                        continue;
+                    }
 
                     beginningOfNumberCoords.push_back(std::pair<int, int>(m, n));
                 }
@@ -80,12 +85,30 @@ int main(int argc, char *argv[])
         }
     }
 
-    std::cout << "Beginning of number coords: " << std::endl;
+    // std::cout << "Beginning of number coords: " << std::endl;
+    // for (const std::pair<int, int> &coords : beginningOfNumberCoords)
+    // {
+    //     std::cout << coords.first << ", " << coords.second << std::endl;
+    // }
+
+    std::vector<int> numbers = {};
     for (const std::pair<int, int> &coords : beginningOfNumberCoords)
     {
-        std::cout << coords.first << ", " << coords.second << std::endl;
+        int row = coords.first;
+        int column = coords.second;
+
+        std::string numberString = "";
+
+        while (column < input[row].size() && std::isdigit(input[row][column]))
+        {
+            numberString += input[row][column];
+            column++;
+        }
+
+        numbers.push_back(std::stoi(numberString));
     }
 
+    const int sum = std::accumulate(numbers.begin(), numbers.end(), 0);
     std::cout << "Result: " << sum << std::endl;
 
     return EXIT_SUCCESS;
